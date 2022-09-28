7982f67fdecf81509240f83726675aed8cda4a03
Christopher Larson <kergoth@gmail.com>
2022-09-28 18:33:19 +0000

    On debian, install gh from feeds
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- components/common.sh
+++ components/common.sh
@@ -12,21 +12,21 @@ has() {
 
 # shellcheck disable=SC3028
 case "${OSTYPE:-}" in
-    darwin*)
-        mac=1
-        debian=0
+darwin*)
+    mac=1
+    debian=0
+    arch=0
+    ;;
+*)
+    mac=0
+    if has apt-get; then
+        debian=1
         arch=0
-        ;;
-    *)
-        mac=0
-        if has apt-get; then
-            debian=1
-            arch=0
-        elif has pacman; then
-            debian=0
-            arch=1
-        fi
-        ;;
+    elif has pacman; then
+        debian=0
+        arch=1
+    fi
+    ;;
 esac
 
 is_mac() {
@@ -75,46 +75,65 @@ apt_install() {
 map_pkg() {
     if [ -z "$NO_SUDO" ] && [ "$debian" -eq 1 ]; then
         case "$1" in
-            ninja)
-                echo ninja-build
-                return
-                ;;
-            fd)
-                echo fd-find
-                return
-                ;;
+        ninja)
+            echo ninja-build
+            return
+            ;;
+        fd)
+            echo fd-find
+            return
+            ;;
         esac
     fi
     echo "$1"
+}
+
+apt_install_gh() {
+    if ! [ -e /usr/share/keyrings/githubcli-archive-keyring.gpg ]; then
+        curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg &&
+            sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+    fi &&
+        if ! [ -e /etc/apt/sources.list.d/github-cli.list ]; then
+            echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null
+        fi &&
+        sudo apt update &&
+        sudo apt install gh -y
 }
 
 pkg_install() {
     pkg="$(map_pkg "$1")"
     if [ "$debian" -eq 1 ]; then
         case "$pkg" in
-            gh | git-delta | shfmt)
-                brew install "$pkg"
-                ;;
-            *)
-                if [ -z "$NO_SUDO" ] && apt-cache show "$pkg" >/dev/null 2>&1; then
-                    apt_install "$pkg"
-                else
-                    brew install "$1"
-                fi
-                ;;
+        gh)
+            if [ -z "$NO_SUDO" ]; then
+                apt_install_gh
+            else
+                brew install gh
+            fi
+            ;;
+        git-delta | shfmt)
+            brew install "$pkg"
+            ;;
+        *)
+            if [ -z "$NO_SUDO" ] && apt-cache show "$pkg" >/dev/null 2>&1; then
+                apt_install "$pkg"
+            else
+                brew install "$1"
+            fi
+            ;;
         esac
     elif [ "$arch" -eq 1 ]; then
         case "$pkg" in
-            gh | pup)
-                brew install "$pkg"
-                ;;
-            *)
-                if [ -z "$NO_SUDO" ]; then
-                    pacman_install "$pkg"
-                else
-                    brew install "$1"
-                fi
-                ;;
+        gh | pup)
+            brew install "$pkg"
+            ;;
+        *)
+            if [ -z "$NO_SUDO" ]; then
+                pacman_install "$pkg"
+            else
+                brew install "$1"
+            fi
+            ;;
         esac
     else
         brew install "$1"
