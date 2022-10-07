198bc121d4dc895d64dc8c2c8dbb925ce67de33a
Christopher Larson <kergoth@gmail.com>
2022-10-07 16:27:25 -0700

    Fix install of powershell modules
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- components/windows/install-pwsh-modules.ps1
+++ components/windows/install-pwsh-modules.ps1
@@ -34,9 +34,9 @@ Function Install-ModuleIfNotInstalled(
 
 $VerbosePreference = "SilentlyContinue"
 
-Install-ModuleIfNotInstalled PSReadLine
-Install-ModuleIfNotInstalled DirColors
-Install-ModuleIfNotInstalled Recycle
-Install-ModuleIfNotInstalled BurntToast
-Install-ModuleIfNotInstalled ZLocation
-Install-ModuleIfNotInstalled posh-alias
+Install-ModuleIfNotInstalled PSReadLine 1.0
+Install-ModuleIfNotInstalled DirColors 1.0
+Install-ModuleIfNotInstalled Recycle 1.0
+Install-ModuleIfNotInstalled BurntToast 1.0
+Install-ModuleIfNotInstalled ZLocation 1.0
+Install-ModuleIfNotInstalled posh-alias 1.0
