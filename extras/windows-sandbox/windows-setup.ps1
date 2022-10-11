87fa396e4ce1e3a8133e0b6b55a9c32ca3c62b62
Christopher Larson <kergoth@gmail.com>
2022-10-11 13:35:50 -0700

    Add windows-sandbox usage example
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

---
+++ extras/windows-sandbox/windows-setup.ps1
@@ -0,0 +1,6 @@
+Set-Location c:\system-setup
+
+$VerbosePreference = 'Continue'
+
+. .\setup-admin.ps1
+runas /trustlevel:0x20000 "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Verbose -NoExit -WindowStyle Maximized -NoProfile -InputFormat None -ExecutionPolicy RemoteSigned -File .\setup.ps1"
