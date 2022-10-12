27f2c224726e6c29deabf29c8db77310bd225419
Christopher Larson <chris_larson@mentor.com>
2022-10-12 23:00:07 +0500

    switch.sh: if we have nvd, use it to show a diff
    Signed-off-by: Christopher Larson <chris_larson@mentor.com>

--- switch.sh
+++ switch.sh
@@ -5,8 +5,14 @@ set -euo pipefail
 configuration="${1:-$(uname -s | tr '[:upper:]' '[:lower:]')}"
 
 cd "$(dirname "$0")"
+generation="$(home-manager generations | head -n 1 | cut -d" " -f7)"
 nix --experimental-features 'nix-command flakes' run .\#homeConfigurations."$configuration".activationPackage
 
 if command -v nixwrap >/dev/null 2>&1; then
     nixwrap
+fi
+
+new_generation="$(home-manager generations | head -n 1 | cut -d" " -f7)"
+if [ "$new_generation" != "generation" ] && command -v nvd >/dev/null 2>&1; then
+    nvd diff "$generation" "$new_generation"
 fi
