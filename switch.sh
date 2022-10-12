c1c67ac431a2de65786b85011d35992c05a8c77c
Christopher Larson <chris_larson@mentor.com>
2022-10-12 22:59:56 +0500

    switch.sh: automatically run nixwrap if we have it
    Signed-off-by: Christopher Larson <chris_larson@mentor.com>

--- switch.sh
+++ switch.sh
@@ -6,3 +6,7 @@ configuration="${1:-$(uname -s | tr '[:upper:]' '[:lower:]')}"
 
 cd "$(dirname "$0")"
 nix --experimental-features 'nix-command flakes' run .\#homeConfigurations."$configuration".activationPackage
+
+if command -v nixwrap >/dev/null 2>&1; then
+    nixwrap
+fi
