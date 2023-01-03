62e2c570bf7a9a18ec6fa2d2903c05a7523721ff
Christopher Larson <chris_larson@mentor.com>
2023-01-03 14:40:11 -0600

    Move nvd into switch.sh, add -n arg to switch.sh
    Signed-off-by: Christopher Larson <chris_larson@mentor.com>

--- build.sh
+++ build.sh
@@ -11,17 +11,4 @@ package=".#homeConfigurations.$configuration.activationPackage"
 
 cd "$(dirname "$0")"
 
-if command -v home-manager >/dev/null 2>&1; then
-    generation="$(home-manager generations | head -n 1 | cut -d" " -f7)"
-else
-    generation="$(nix path-info "$package" 2>/dev/null)" || :
-fi
 nix build --no-link "$package"
-new_generation="$(nix path-info "$package" 2>/dev/null)" || :
-PATH="$new_generation/home-path/bin:$PATH"
-
-if command -v nvd >/dev/null 2>&1; then
-    if [ -n "$generation" ] && [ -n "$new_generation" ] && [ "$generation" != "$new_generation" ]; then
-        nvd diff "$generation" "$new_generation"
-    fi
-fi
--- switch.sh
+++ switch.sh
@@ -1,10 +1,38 @@
 #!/bin/bash
-#
+
+usage() {
+    echo >&2 "${0##*/} [-n] [CONFIGURATION]"
+    exit 2
+}
+
 nix () {
     command nix --experimental-features 'nix-command flakes' "$@"
 }
 
+if ! command -v home-manager &>/dev/null; then
+    hm () {
+        nix run --no-write-lock-file github:nix-community/home-manager/ -- "$@"
+    }
+else
+    hm () {
+        home-manager "$@"
+    }
+fi
+
 set -euo pipefail
+
+dry_run=
+while getopts nh opt; do
+    case "$opt" in
+        n)
+            dry_run=1
+            ;;
+        \? | h)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND - 1))
 
 if [ $# -eq 0 ]; then
     configuration=$(uname -s | tr '[:upper:]' '[:lower:]')
@@ -15,16 +43,20 @@ fi
 
 cd "$(dirname "$0")"
 ./build.sh "$configuration"
-if ! command -v home-manager &>/dev/null; then
-    hm () {
-        nix run --no-write-lock-file github:nix-community/home-manager/ -- "$@"
-    }
-else
-    hm () {
-        home-manager "$@"
-    }
+
+generation="$(hm generations | head -n 1 | cut -d" " -f7)" || :
+if [ -n "$generation" ]; then
+    package=".#homeConfigurations.$configuration.activationPackage"
+    new_generation="$(nix path-info "$package" 2>/dev/null)" || :
+
+    if command -v nvd >/dev/null 2>&1; then
+        if [ -n "$generation" ] && [ -n "$new_generation" ] && [ "$generation" != "$new_generation" ]; then
+            nvd diff "$generation" "$new_generation"
+        fi
+    fi
 fi
-hm --flake ".#$configuration" switch
+
+hm --flake ".#$configuration" switch ${dry_run:+-n}
 
 if command -v nixwrap >/dev/null 2>&1; then
     nixwrap
